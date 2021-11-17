73b8a575ec47148f361220246d2936447d1c683a
MARECHAL Armand <Armand.MARECHAL@cpnv.ch>
2021-11-17 13:26:24 +0100

    Card Animation

--- src/resources/views/livewire/best-practice-card.blade.php
+++ src/resources/views/livewire/best-practice-card.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col p-6 hover:scale-105 hover:cursor-pointer transition duration-300 bg-gray-100 rounded-xl shadow">
+<div class="flex flex-col p-6 hover:scale-105 hover:cursor-pointer transition duration-300 bg-gray-100 rounded-xl shadow animate-fade-in-down">
     <h2 class="my-2 font-bold text-xl"> {{ $bestPractice->name }}</h2>
     <p class="my-2 flex-grow"> {{ Str::of($bestPractice->description)->words(35) }} </p>
     <button wire:click="delete" class="px-4 py-2 w-full bg-red-500 uppercase font-bold text-sm text-white rounded">Supprimer</button>
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     mode: 'jit',
@@ -6,16 +6,29 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/views/**/*.blade.php'
     ],
-
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans]
             },
-        },
+            keyframes: {
+                'fade-in-down': {
+                    '0%': {
+                        opacity: '0',
+                        transform: 'translateY(-10px)'
+                    },
+                    '100%': {
+                        opacity: '1',
+                        transform: 'translateY(0)'
+                    }
+                }
+            },
+            animation: {
+                'fade-in-down': 'fade-in-down 0.5s ease-out'
+            }
+        }
     },
-
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-};
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
+}
