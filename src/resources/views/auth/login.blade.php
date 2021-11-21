154fcb73261e7f66778a24427e068a21f4a5073d
Penfu <sypenfu@gmail.com>
2021-11-21 17:20:31 +0100

    Update Auth Views

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,15 +1,12 @@
 <x-guest-layout>
     <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
+        <x-slot name="logo"></x-slot>
         <x-jet-validation-errors class="mb-4" />
 
         @if (session('status'))
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ session('status') }}
-        </div>
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
         @endif
 
         <form method="POST" action="{{ route('login') }}">
@@ -17,12 +14,14 @@
 
             <div>
                 <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
@@ -34,9 +33,10 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                        href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
                 @endif
 
                 <x-jet-button class="ml-4">
@@ -44,11 +44,10 @@
                 </x-jet-button>
             </div>
             <div class="flex flex-col gap-y-2 mt-4 text-center">
-                <a href="{{ route('login.github') }}" class="px-4 py-2 w-full bg-gray-800 rounded text-white">Log in with GitHub</a>
-                <a href="{{ route('login.google') }}" class="px-4 py-2 w-full bg-white rounded border border-gray-300 text-black">Log in with Google</a>
+                <a href="{{ route('login.github') }}" class="px-4 py-2 w-full bg-gray-800 rounded text-white">Log in
+                    with GitHub</a>
             </div>
         </form>
     </x-jet-authentication-card>
-
 
 </x-guest-layout>
