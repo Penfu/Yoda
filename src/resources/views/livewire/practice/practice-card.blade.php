5569bbef64f2cb3ff00e1e02396dbb6d2eb60cea
Penfu <sypenfu@gmail.com>
2021-12-06 19:41:22 +0100

    Update Home View

--- src/resources/views/livewire/practice/practice-card.blade.php
+++ src/resources/views/livewire/practice/practice-card.blade.php
@@ -1,12 +1,12 @@
 <a href="{{ route('practice', ['practice' => $practice->id]) }}">
     <div
-        class="flex flex-col h-full p-6 hover:scale-105 hover:cursor-pointer transition duration-300 bg-gray-100 rounded-xl shadow animate-fade-in-down">
+        class="flex flex-col h-full p-6 group hover:cursor-pointer transition duration-300 bg-white rounded-md shadow hover:shadow-lg animate-fade-in-down">
         @empty(session('domain'))
-            <h2 class="my-2 font-bold text-xl">{{ $practice->domain->name }}</h2>
+            <h2 class="my-2 font-bold text-xl text-gray-900 uppercase">{{ $practice->domain->name }}</h2>
         @endempty
 
-        <p class="my-2 flex-grow">{{ Str::of($practice->description)->words(35) }}</p>
-        <span class="justify-self-end text-right">
+        <p class="my-2 flex-grow text-gray-700 group-hover:text-gray-900">{{ Str::of($practice->description)->words(35) }}</p>
+        <span class="justify-self-end text-right italic">
             Modifié le {{ $practice->updated_at->translatedFormat('j F Y') }}
         </span>
     </div>
--- src/resources/views/livewire/practice/practice-container.blade.php
+++ src/resources/views/livewire/practice/practice-container.blade.php
@@ -1,18 +1,21 @@
 <div class="py-4">
     @isset($days)
-        <div class="pb-12">
-            <span class="">Nouveau de</span>
+        <div class="pb-4">
+            <span class="font-bold text-2xl">Pratique modifié depuis {{ $days != 1 ? 'les' : null }}</span>
             <input wire:model="days" name="updated-limit" type="number" min="1" max="1000" value="{{ $days }}"
-                class="w-24 h-8 rounded"></input>
-            <span class="">{{ $days == 1 ? 'jour' : 'jours' }}</span>
+                class="w-20 h-8 rounded"></input>
+            <span class="font-bold text-2xl">{{ $days == 1 ? 'jour' : 'derniers jours' }}</span>
+            @if (count($practices) > 0)
+                <p>{{ count($practices) }} pratiques trouvés</p>
+            @else
+                <p>Aucune pratique trouvé</p>
+            @endif
         </div>
     @endisset
 
     <div class="grid grid-cols-1 gap-y-4 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8 transition duration-1000">
-        @forelse ($practices as $practice)
+        @foreach ($practices as $practice)
             <livewire:practice.practice-card :practice="$practice" :key="$practice->id" />
-        @empty
-            <h3>Aucune pratique à afficher ici</h3>
-        @endforelse
+        @endforeach
     </div>
 </div>
